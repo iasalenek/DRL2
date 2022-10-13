6c150afab5d06972b10823dfedee967c4dac9e29
Иван Саленек <ivansalenek@MacBook-Air-Ivan.local>
2022-10-13 16:03:03 +0300

    slightly clean repo

--- HW1/agent.py
+++ HW1/agent.py
@@ -1,43 +1,26 @@
+import copy
 import numpy as np
 import torch
-from my_utils import compute_observation
+from my_utils.common_utils import calc_distance_map
+from my_utils.dqn_utils import compute_observation
 
-import matplotlib.pyplot as plt
-import time
-
-DEVICE = 'cpu'
-
-class Agent():
-
-    def __init__(self, num_predators: int=5):
+class Agent:
+    def __init__(self):
         self.model = torch.load("agent.pkl", map_location='cpu')
-        self.num_predators = num_predators
+        self.model.eval()
 
     def get_actions(self, state, info):
-        obs = compute_observation(state, self.num_predators)
-        obs = np.expand_dims(obs, axis=0)
 
-        action = self.model(torch.Tensor(obs).to(DEVICE)).view(self.num_predators, 5).argmax(axis = 1)
+        observations = []
+        for agent_id in range(5):
+            obs = compute_observation(state, agent_id, self.distance_map)
+            observations.append(obs)
 
-        action = action.to(int).cpu().detach().tolist()
-        print(action)
-        #print(self.model(torch.Tensor(obs).to(DEVICE)))
-        #time.sleep(5)
-        return action
-        #return [0, 0, 0, 0, 0]
+        observations = np.array(observations)
+        
+        actions = self.model(torch.Tensor(observations)).argmax(axis=1)
+        
+        return actions.to(int).cpu().detach().tolist()
 
     def reset(self, state, info):
-        # TODO: implement
-        pass
-
-
-# Test
-
-
-# num_predators = 5
-# obs = compute_observation(state, num_predators)
-# obs = np.expand_dims(obs, axis=0)
-
-# model(torch.Tensor(obs).to('cpu')).view(num_predators, 5)
-
-# action = model(torch.Tensor(obs).to('cpu')).view(num_predators, 5).argmax(axis = 1)
+        self.distance_map = calc_distance_map(state)
--- HW1/train.sh
+++ HW1/train.sh
@@ -1,5 +1,2 @@
-python my_dqn.py \
---batch_size 16 \
---eval_every 1000 \
---episodes 5 \
---num_predators 5
+!python dqn.py --batch_size 16 --initial_steps 10000 --eval_every 10000 \
+--transitions 300000 --epsilon 0.2 --lr 0.0005
