00d216ff4af8707fb1a0a171b4bf7975c0cd4ae8
Иван Саленек <ivansalenek@MacBook-Air-Ivan.local>
2022-10-26 02:11:37 +0300

    change net

--- HW3/nets.py
+++ HW3/nets.py
@@ -9,12 +9,13 @@ class Slava_net(nn.Module):
         self.conv1 = self._get_conv_block(in_channels, 16 - in_channels)
         self.conv2 = self._get_conv_block(16, 32 - in_channels)
         self.conv3 = self._get_conv_block(32, 64 - in_channels)
+        self.conv4 = self._get_last_conv_block(64, 128)
 
         self.maxpool = nn.MaxPool2d(2)
 
         self.fc = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(5 * 5 * 64, 5)
+            nn.Linear(128, 5)
         )
 
     def forward(self, input):
@@ -25,6 +26,7 @@ class Slava_net(nn.Module):
         x = torch.cat((x, input[:, :, 15:25, 15:25]), dim=1)
         x = self.maxpool(self.conv3(x))
         x = torch.cat((x, input[:, :, 18:23, 18:23]), dim=1)
+        x = self.conv4(x)
         x = self.fc(x)
 
         return x
@@ -36,7 +38,22 @@ class Slava_net(nn.Module):
             nn.Conv2d(c_in, c_out, 3, 1, 1),
             nn.ReLU(),
             nn.Conv2d(c_out, c_out, 3, 1, 1),
+            nn.
+            ReLU(),
+        )
+
+        return block
+    
+    @staticmethod
+    def _get_last_conv_block(c_in, c_out):
+
+        block = nn.Sequential(
+            nn.Conv2d(c_in, c_out, 3, 1, 0),
+            nn.ReLU(),
+            nn.Conv2d(c_out, c_out, 3, 1, 0),
             nn.ReLU(),
         )
 
         return block
+
+        
